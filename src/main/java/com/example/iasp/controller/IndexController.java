24167f6e7cf1291b54c19c88eadb6bd00e6709c6
yanghongwang <hongwang.yang@newtouch.com>
2020-12-14 21:38:50 +0800

    新增IndexController

---
+++ src/main/java/com/example/iasp/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.example.iasp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: Y.H.W
+ * @Date: 2020/12/14 21:26
+ * @Description: com.example.iasp.controller
+ */
+@RestController
+public class IndexController {
+}
